900d5d9714756247de30c5899d2a9318687a8ca7
EujinKo <42593556+EujinKo@users.noreply.github.com>
2020-10-09 06:47:22 -0700

    Updates

---
+++ precache-manifest.35b3c7fe8cea5c52f164dc1b0873a5fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc91fa29928edcfd005814033602897",
+    "url": "/imagequiz/index.html"
+  },
+  {
+    "revision": "73293db4b415e09a0061",
+    "url": "/imagequiz/static/css/2.a4e66202.chunk.css"
+  },
+  {
+    "revision": "a587b0e8ef5e23245bc8",
+    "url": "/imagequiz/static/css/main.8e69e4e8.chunk.css"
+  },
+  {
+    "revision": "73293db4b415e09a0061",
+    "url": "/imagequiz/static/js/2.aabb3bd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imagequiz/static/js/2.aabb3bd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a587b0e8ef5e23245bc8",
+    "url": "/imagequiz/static/js/main.fe288cf0.chunk.js"
+  },
+  {
+    "revision": "85da51fa7abe07fb0938",
+    "url": "/imagequiz/static/js/runtime-main.2f3f5ace.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/imagequiz/precache-manifest.c5be4b5f7c1299ae4482327a5654e150.js"
+  "/imagequiz/precache-manifest.35b3c7fe8cea5c52f164dc1b0873a5fc.js"
 );
 
 self.addEventListener('message', (event) => {
