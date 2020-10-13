b21e8e1eea72a77c24d1ccbda2a85a62a95e7de2
EujinKo <42593556+EujinKo@users.noreply.github.com>
2020-10-13 11:36:04 -0700

    Added routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import Home from './components/Home';
 import './App.css';
 
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Home />
+    // <div className="App">
+    //   <Home />
       
-    </div>
+    // </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route exact path='/login'>
+        {/* <Login /> */}
+      </Route>
+    </switch>
+    </BrowserRouter>
   );
 }
 
