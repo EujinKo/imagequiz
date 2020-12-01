f68b6160cec62f48c73cbb92d5744d3219e9a55d
EujinKo <42593556+EujinKo@users.noreply.github.com>
2020-12-01 08:59:36 -0700

    Update server.js

--- src/ServerInterface/server.js
+++ src/ServerInterface/server.js
@@ -1,8 +1,18 @@
-import entries from './entries';
+// import entries from './entries';
+let api = "https://eujinko-imagequiz.herokuapp.com/quizzes";
 
 let getEntries = () =>{
     //Later on we will connect to a backend and fetch all the entries
-    return entries;
+
+    fetch(api)
+    .then(x => {
+        // console.log(x);
+        return x.json();
+    })
+    .then( y => console.log(y))
+    .catch(e=>console.log(e));
+
+    // return entries;
 }
 
 let server = {
